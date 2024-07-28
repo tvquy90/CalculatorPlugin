fb3d32b6eeb98a888ea980d83fc5071ecd3d43e9
unknown <tvquy.vn@gmail.com>
2024-07-28 22:42:20 +0700

    d

--- www/CalculatorPlugin.js
+++ www/CalculatorPlugin.js
@@ -1,17 +1,20 @@
-var exec = require('cordova/exec');
 
-exports.getAddResult = function (arg0,arg1, success, error) {
-    exec(success, error, 'CalculatorPlugin', 'add', [arg0,arg1]);
-};
+var CalculatorPlugin = {
 
-exports.getSubResult = function (arg0,arg1, success, error) {
-    exec(success, error, 'CalculatorPlugin', 'sub', [arg0,arg1]);
-};
+	getAddResult:function (arg0,arg1, success, error) {
+		cordova.exec(success, error, 'CalculatorPlugin', 'add', [arg0,arg1]);
+	},
 
-exports.getMulResult = function (arg0,arg1, success, error) {
-    exec(success, error, 'CalculatorPlugin', 'mul', [arg0,arg1]);
-};
+	getSubResult:function (arg0,arg1, success, error) {
+		cordova.exec(success, error, 'CalculatorPlugin', 'sub', [arg0,arg1]);
+	},
 
-exports.getDivResult = function (arg0,arg1, success, error) {
-    exec(success, error, 'CalculatorPlugin', 'div', [arg0,arg1]);
-};
+	getMulResult:function (arg0,arg1, success, error) {
+		cordova.exec(success, error, 'CalculatorPlugin', 'mul', [arg0,arg1]);
+	},
+
+	getDivResult:function (arg0,arg1, success, error) {
+		cordova.exec(success, error, 'CalculatorPlugin', 'div', [arg0,arg1]);
+	}
+}
+module.exports = CalculatorPlugin;
